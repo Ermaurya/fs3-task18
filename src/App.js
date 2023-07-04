6bab3f40d26670176f079f287cdeef3928e516d8
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2023-07-05 00:54:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Nav from "./Components/Nav/Nav";
+
+
+import Form from "./Components/Form/Form";
+
+function App(props) {
+  return (
+    <div className="App">
+      <Nav />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
